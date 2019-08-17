3ced393132c4a170714a4febeab039f280987c8a
N8 <n8@n8programs.com>
2019-08-17 21:24:00 +0200

    Mobile mode

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 import highlight from "./zhighlight.js";
 
+const menu = document.getElementById("menu");
+const nav = document.getElementById("nav");
+const overlay = document.getElementById("overlay");
+menu.addEventListener("click", () => {
+    menu.style.display = "none";
+    nav.classList.add("w3-animate-left");
+    nav.style.display = "block";
+    setTimeout(() => {
+        overlay.style.display = "block";
+    }, 400);
+})
+overlay.addEventListener("click", () => {
+    menu.style.display = "block";
+    nav.style.display = "none";
+    overlay.style.display = "none";
+});
 window.onload = highlight;
